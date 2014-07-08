3fcc94207118cafe371f37c96b2f7b9454855420
drdrej <andrej.siebert@googlemail.com>
2014-07-08 15:15:14 +0200

    fix: package.json - main file

--- test_exec_cli.js
+++ test_exec_cli.js
@@ -1,5 +1,6 @@
 // Example: to create artefacts :::
 
 //var impl = require( "../impl/main.js" );
+var S = require( 'string' );
 var impl = require( "artefactjs" );
 impl.artefact(  "./TestEnum.json" );
